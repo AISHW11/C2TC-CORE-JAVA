245a48a8dc927977ce8289226962d8fd5ca279d8
HP <HP@192.168.0.102>
2022-03-09 15:12:50 +0530

    If_eles_Stmt

---
+++ M11/src/traning/If_else_stmt.java
@@ -0,0 +1,21 @@
+package traning;
+
+public class If_else_stmt {
+
+	public static void main(String[] args) {
+			int x=30;
+			if(x==20)
+			{
+				System.out.println("Good morning");
+			}
+			
+			else
+				
+			{
+				System.out.println("Good noon");
+			}
+
+		}
+	
+
+	}
