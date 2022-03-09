401774a2dafa32aa6d8bd1a5f4dd881fecdcfcaf
HP <HP@192.168.0.102>
2022-03-09 15:15:09 +0530

    Switch_Stmt

---
+++ M11/src/traning/Switch_Stmt.java
@@ -0,0 +1,22 @@
+package traning;
+
+public class Switch_Stmt {
+
+	public static void main(String[] args) 
+	{
+		int x=11;
+		switch(x)
+		{
+		case 10:
+			System.out.println("Good Friday");
+			break;
+			default:
+				System.out.println("Friday");
+				
+				
+		}
+
+	}
+
+}
+ 
