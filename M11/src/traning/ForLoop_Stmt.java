f9376079e94a4c63ac3d47404050a6bcbfbf7e29
HP <HP@192.168.0.102>
2022-03-09 16:00:27 +0530

    ForLoop_Stmt

---
+++ M11/src/traning/ForLoop_Stmt.java
@@ -0,0 +1,14 @@
+package traning;
+
+public class ForLoop_Stmt {
+
+	public static void main(String[] args) {
+		int x=0;
+		for(System.out.println("Core Java");x<3;x++)
+		{
+			System.out.println("Advance Java");
+		}
+
+	}
+
+}
