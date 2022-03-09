9dfa5aedcb68007be7e4f38c4bf17a1592ae09ed
HP <HP@192.168.0.102>
2022-03-09 15:13:48 +0530

    Student

---
+++ M11/src/traning/student.java
@@ -0,0 +1,25 @@
+package traning;
+
+public class student {
+	String name;
+	int age,roll;
+	
+
+	public static void main(String[] args) {
+		System.out.println("STUDENT 1 DETAILS");
+		student s1=new student();
+		System.out.println(s1.name);
+		System.out.println(s1.age);
+		System.out.println(s1.roll);
+		
+		System.out.println("STUDENT 2 DETAILS");
+		student s2=new student();
+		System.out.println(s2.name);
+		System.out.println(s2.age);
+		System.out.println(s2.roll);
+		
+		
+
+	}
+
+}
