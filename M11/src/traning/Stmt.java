203886137a95398db66bb65f42a9ec6e5aca911b
HP <HP@192.168.0.102>
2022-03-10 21:12:48 +0530

    Stmt

---
+++ M11/src/traning/Stmt.java
@@ -0,0 +1,22 @@
+package traning;
+
+public class Stmt {
+
+	public static void main(String[] args) {
+		int x=0;
+		do
+		{
+			x++;
+			System.out.println(x);
+			
+			if(++x<5)
+				continue;
+			
+			x++;
+			System.out.println(x);
+			
+		}while(++x<10);
+
+	}
+
+}
