2c7e5572e5bfdb37634036d0df761f3332a88cae
HP <HP@192.168.0.102>
2022-03-10 21:29:40 +0530

    Break_Stmt

---
+++ M11/src/traning/Break_Stmt.java
@@ -0,0 +1,15 @@
+package traning;
+
+public class Break_Stmt {
+
+	public static void main(String[] args) {
+		for (int i=0;i<10;i++)
+		{
+			if(i==5)
+				break;
+			System.out.println(i);
+		}
+
+	}
+
+}
