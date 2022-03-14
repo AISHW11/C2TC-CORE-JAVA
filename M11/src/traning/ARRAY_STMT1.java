3967638f7f57a3e2e50d9937d8755ccf69fb8c0e
HP <HP@192.168.0.102>
2022-03-14 16:08:33 +0530

    ARRAY_STMT1

---
+++ M11/src/traning/ARRAY_STMT1.java
@@ -0,0 +1,23 @@
+package traning;
+
+public class ARRAY_STMT1 {
+
+	public static void main(String[] args) {
+		 int [][] x=new int[2][3];
+		  
+		 System.out.println(x);
+		 
+		 System.out.println(x[0]);
+		 
+		 System.out.println(x[0][0]);
+		 
+		 System.out.println(x[0][1]);
+		 
+		 System.out.println(x[1][0]);
+		 
+		 System.out.println(x[1][1]);
+		 
+
+	}
+
+}
