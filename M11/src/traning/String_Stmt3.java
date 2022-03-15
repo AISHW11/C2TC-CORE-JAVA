2a180fb5faa94c2916239856a8aee8840760c63a
HP <HP@192.168.0.102>
2022-03-15 11:31:55 +0530

    String_Stmt3

---
+++ M11/src/traning/String_Stmt3.java
@@ -0,0 +1,14 @@
+package traning;
+
+public class String_Stmt3 {
+
+	public static void main(String[] args) {
+		String s="Aishwarya";
+		System.out.println(s.length());
+		
+		String upper=s.toUpperCase();
+		System.out.println(upper);
+
+	}
+
+}
