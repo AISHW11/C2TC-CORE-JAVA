d509beb0d8cc055fc23dee8c0dc9049dcdbf4a78
HP <HP@192.168.0.102>
2022-03-15 11:41:46 +0530

    String_Stmt6

---
+++ M11/src/traning/String_Stmt5.java
@@ -0,0 +1,18 @@
+package traning;
+
+public class String_Stmt5 {
+
+	public static void main(String[] args) {
+		String s="Aishwarya";
+		System.out.println(s.length());
+		
+		String upper=s.toUpperCase();
+		System.out.println(upper);
+		
+		System.out.println(s.substring(2));
+		System.out.println(s.substring(1, 7));
+		
+
+	}
+
+}
