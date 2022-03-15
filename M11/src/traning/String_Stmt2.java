1d028f9ef9023c8273f6995af9e0e2ffd3b28c20
HP <HP@192.168.0.102>
2022-03-15 11:27:57 +0530

    String_Stmt2

---
+++ M11/src/traning/String_Stmt2.java
@@ -0,0 +1,15 @@
+package traning;
+
+public class String_Stmt2 {
+
+	public static void main(String[] args) {
+		String s="Aishwarya";
+		System.out.println(s.length());
+		
+		String lower=s.toLowerCase();
+		System.out.println(lower);
+		
+
+	}
+
+}
