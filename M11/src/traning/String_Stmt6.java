cea32389a04582a75174660eeb7fc03363568b38
HP <HP@192.168.0.102>
2022-03-15 11:45:44 +0530

    String_Stmt6

---
+++ M11/src/traning/String_Stmt6.java
@@ -0,0 +1,19 @@
+package traning;
+
+public class String_Stmt6 {
+
+	public static void main(String[] args) {
+		String a="Java";
+		int b=10,c=20,d=30;
+		
+		System.out.println(a+b+c+d);
+		
+		System.out.println(b+c+d+a);
+		
+		System.out.println(b+c+a+d);
+		
+		System.out.println(b+a+c+d);
+
+	}
+
+}
