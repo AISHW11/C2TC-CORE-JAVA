a99e46e55f31f74dd89a282d4cad76c7305ecdbe
HP <HP@192.168.0.102>
2022-03-15 11:36:53 +0530

    String_Stmt4

---
+++ M11/src/traning/String_Stmt4.java
@@ -0,0 +1,15 @@
+package traning;
+
+public class String_Stmt4 {
+
+	public static void main(String[] args) {
+		String s="Aishwarya";
+		System.out.println(s.length());
+		
+		String upper=s.toUpperCase();
+		System.out.println(upper);
+		
+		System.out.println(s.substring(2));
+	}
+
+}
