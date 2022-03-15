ffadf64d51c748fab7fa84b8e4d22994e0c79c3a
HP <HP@192.168.0.102>
2022-03-15 11:24:06 +0530

    String_Stmt

---
+++ M11/src/traning/String_stmt.java
@@ -0,0 +1,12 @@
+package traning;
+
+public class String_stmt {
+
+	public static void main(String[] args) {
+		StringBuffer s =new StringBuffer("Aishwarya");
+		s.append(" Waghmare");
+		System.out.println(s);
+
+	}
+
+}
