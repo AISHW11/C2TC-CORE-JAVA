5b61f76fb9d3fc5c799460c3d86bf65585a9a68e
HP <HP@192.168.0.101>
2022-03-21 13:57:24 +0530

    Public_Access_Modifier

---
+++ M11/src/p6/A.java
@@ -0,0 +1,12 @@
+package p6;
+
+public class A 
+{
+
+	public void display() 
+	{
+		System.out.println("TNS Sessions");
+	
+	}
+
+}
---
+++ M11/src/p7/B.java
@@ -0,0 +1,14 @@
+package p7;
+import p6.*;
+
+public class B 
+{
+
+	public static void main(String[] args) 
+	{
+		A obj = new A();
+		obj.display();
+
+	}
+
+}
