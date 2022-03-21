99b0f3e96180b620a601cd8afb6528a022abcb39
HP <HP@192.168.0.101>
2022-03-21 13:53:41 +0530

    Protected_Access_Modifier

---
+++ M11/src/p4/A.java
@@ -0,0 +1,12 @@
+package p4;
+
+public class A 
+{
+
+	protected void display() 
+	{
+		System.out.println("TNS Sessions");
+
+	}
+
+}
---
+++ M11/src/p5/B.java
@@ -0,0 +1,14 @@
+package p5;
+import p4.*;
+
+public class B extends A 
+{
+
+	public static void main(String[] args) 
+	{
+		B obj = new B();
+		obj.display();
+
+	}
+
+}
