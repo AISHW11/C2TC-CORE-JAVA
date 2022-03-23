cd639808479cecd838544415f47d602728a5cac5
HP <HP@192.168.0.102>
2022-03-23 10:55:45 +0530

    Static_block

---
+++ M11/src/keywords/Static_block.java
@@ -0,0 +1,23 @@
+package keywords;
+
+public class Static_block {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi Aish");
+
+	}
+	static 
+	{
+		System.out.println("Good Morning");
+	}
+	static 
+	{
+		System.out.println("Good Noon");
+	}
+	static 
+	{
+		System.out.println("Good Night");
+	}
+
+}
